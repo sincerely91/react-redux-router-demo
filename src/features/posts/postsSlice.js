c5eab2e33995ec39af1bd346833f18e33b220669
blockchaindev91 <hisamitsuteru@gmail.com>
2022-01-18 18:47:43 +0530

    setting up store

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,25 +1,12 @@
 import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
-import { client } from '../../api/client'
 
 const initialState = {
-    posts: [],
+    posts: [
+      {id: 1, title: "lorem ipsum", content: "lorem ipsum conent"}
+    ],
     status: 'idle',
     error: null 
 }
-
-export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-    const response = await client.get('/fakeApi/posts')
-    console.log(response.data)
-    return response.data
-})
-
-export const addNewPost = createAsyncThunk(
-    'posts/addNewPost',
-    async (initialPost) => {
-      const response = await client.post('/fakeApi/posts', initialPost)
-      return response.data
-    }
-  )
 
 const postsSlice = createSlice({
     name: 'posts',
@@ -36,25 +23,7 @@ const postsSlice = createSlice({
                     existingPost.content = content
                 }
             }
-        },
-        extraReducers(builder) {
-            builder
-              .addCase(fetchPosts.pending, (state, action) => {
-                state.status = 'loading'
-              })
-              .addCase(fetchPosts.fulfilled, (state, action) => {
-                state.status = 'succeeded'
-                // Add any fetched posts to the array
-                state.posts = state.posts.concat(action.payload)
-              })
-              .addCase(fetchPosts.rejected, (state, action) => {
-                state.status = 'failed'
-                state.error = action.error.message
-              })
-              .addCase(addNewPost.fulfilled, (state, action) => {
-                state.posts.push(action.payload)
-              })
-        },
+        }
         
     }
 })
