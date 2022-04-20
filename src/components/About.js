87f678c243f7205c26a807435fcbf82be3cc833c
blockchaindev91 <hisamitsuteru@gmail.com>
2022-04-20 17:49:06 +0530

    some dummpy text added

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
 const About = () => {
+    if (navigator.onLine) {
+        console.log('online');
+    } else {
+        console.log('offline');
+    }
+
+    // const text = 'Proin ex diam, ultrices ut risus non, https://www.google.com dapibus accumsan nulla. Curabitur in vestibulum tortor.'
+
+    // const linkify = (text) => {
+    //     var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    //     return text.replace(urlRegex, function(url) {
+    //         return '<a href="' + url + '">' + url + '</a>';
+    //     });
+    // }
 
     return (
         <div className='container'>
             <h1>About Us</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper placerat metus sit amet tempor. Ut fermentum, elit eget euismod auctor, felis metus ultrices tellus, vel imperdiet metus sem at justo. Donec imperdiet suscipit ipsum ut pellentesque. Nam nunc ipsum, laoreet non ornare sit amet, scelerisque quis nulla. Aenean eu ipsum at enim imperdiet pharetra. Fusce lacinia nec massa quis tincidunt. Nulla auctor, tortor pretium varius porttitor, felis elit consectetur tellus, non ullamcorper metus libero in tellus. Sed tempor hendrerit arcu vel ornare. Curabitur vitae vulputate sapien. Ut pulvinar augue elit, vel bibendum metus bibendum ut. Proin ex diam, ultrices ut risus non, dapibus accumsan nulla. Curabitur in vestibulum tortor.</p>
+            <p>Aenean eu ipsum at enim imperdiet pharetra. Fusce lacinia nec massa quis tincidunt. Nulla auctor, tortor pretium varius porttitor, felis elit consectetur tellus, non ullamcorper metus libero in tellus. Sed tempor hendrerit arcu vel ornare. Curabitur vitae vulputate sapien. Ut pulvinar augue elit, vel bibendum metus bibendum ut. Proin ex diam, ultrices ut risus non, dapibus accumsan nulla. Curabitur in vestibulum tortor.</p>      
+
+            
         </div>        
     )
 }
