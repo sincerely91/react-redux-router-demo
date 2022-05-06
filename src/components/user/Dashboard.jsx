ddacd670e0049a6b3dc86e57c8823f2f0a86baef
blockchaindev91 <hisamitsuteru@gmail.com>
2022-05-06 18:34:02 +0530

    some fixed

--- src/components/user/Dashboard.jsx
+++ src/components/user/Dashboard.jsx
@@ -6,6 +6,7 @@ const Dashboard = () => {
         <div className='container-fluid'>
             <div className='row'>
                 <h1>Dashboard</h1>
+                <p>This is dashboard page.</p>
             </div>
         </div>
     );
